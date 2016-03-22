be0aabd10367364a32b573526cce7ccff37eda3f
Payton Swick <payton@foolord.com>
2016-03-22 12:05:14 -0400

    Move helpers into Spies namespace

--- spies/globals.php
+++ spies/globals.php
@@ -1,31 +1,26 @@
 <?php
+namespace Spies;
 
-if ( ! function_exists( 'mock_function' ) ) {
-	function mock_function( $function_name ) {
-		return \Spies\Spy::stub_function( $function_name );
-	}
+function stub_function( $function_name ) {
+	return \Spies\Spy::stub_function( $function_name );
 }
 
-if ( ! function_exists( 'get_spy_for' ) ) {
-	function get_spy_for( $function_name ) {
-		return \Spies\Spy::get_spy_for( $function_name );
-	}
+function mock_function( $function_name ) {
+	return \Spies\Spy::stub_function( $function_name );
 }
 
-if ( ! function_exists( 'expect_spy' ) ) {
-	function expect_spy( $spy ) {
-		return \Spies\Expectation::expect_spy( $spy );
-	}
+function get_spy_for( $function_name ) {
+	return \Spies\Spy::get_spy_for( $function_name );
 }
 
-if ( ! function_exists( 'finish_spying' ) ) {
-	function finish_spying() {
-		return \Spies\Expectation::finish_spying();
-	}
+function expect_spy( $spy ) {
+	return \Spies\Expectation::expect_spy( $spy );
 }
 
-if ( ! function_exists( 'mock_object' ) ) {
-	function mock_object() {
-		return \Spies\MockObject::mock_object();
-	}
+function finish_spying() {
+	return \Spies\Expectation::finish_spying();
+}
+
+function mock_object() {
+	return \Spies\MockObject::mock_object();
 }
