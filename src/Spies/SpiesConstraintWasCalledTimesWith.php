8247c8e1f24403a6039fbe91f6fe1996199f942e
Payton Swick <payton@foolord.com>
2016-08-08 18:40:08 -0400

    Add SpiesConstraintWasCalledTimesWith

---
+++ src/Spies/SpiesConstraintWasCalledTimesWith.php
@@ -0,0 +1,38 @@
+<?php
+namespace Spies;
+
+class SpiesConstraintWasCalledTimesWith extends \PHPUnit_Framework_Constraint {
+	private $expected_args;
+	private $count;
+
+	public function __construct( $count, $args ) {
+		parent::__construct();
+		$this->expected_args = $args;
+		$this->count = $count;
+	}
+
+	public function matches( $other ) {
+		if ( ! $other instanceof \Spies\Spy ) {
+			return false;
+		}
+		return $other->was_called_times_with( $this->count, $this->expected_args );
+	}
+
+	protected function failureDescription( $other ) {
+		$generator = new FailureGenerator();
+		$generator->spy_was_not_called_with( $other, $this->expected_args );
+		return $generator->get_message();
+	}
+
+	protected function additionalFailureDescription( $other ) {
+		$generator = new FailureGenerator();
+		$generator->spy_was_not_called_with_additional( $other );
+		return $generator->get_message();
+	}
+
+	public function toString() {
+		return '';
+	}
+}
+
+
