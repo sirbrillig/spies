bf0fae81ad0984cc41fc938445d13f1ec1785665
Payton Swick <payton@foolord.com>
2017-09-25 16:44:39 -0400

    Add MatchPattern class

---
+++ src/Spies/MatchPattern.php
@@ -0,0 +1,21 @@
+<?php
+namespace Spies;
+
+class MatchPattern {
+	public function __construct( $pattern ) {
+		$this->expected_pattern = $pattern;
+	}
+
+	public function is_match( $actual ) {
+		if ( ! is_string( $actual ) ) {
+			return false;
+		}
+		// Convert 0 and FALSE to false and 1 to true
+		if ( preg_match( $this->expected_pattern, $actual ) ) {
+			return true;
+		}
+		return false;
+	}
+}
+
+
