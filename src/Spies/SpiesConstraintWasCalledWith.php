dc7b321ba714db7c664908cb40f984d7eadd9973
Payton Swick <payton@foolord.com>
2017-10-24 18:21:39 -0400

    SpiesConstraintWasCalledWith: add negation

--- src/Spies/SpiesConstraintWasCalledWith.php
+++ src/Spies/SpiesConstraintWasCalledWith.php
@@ -3,22 +3,27 @@ namespace Spies;
 
 class SpiesConstraintWasCalledWith extends BaseConstraint {
 	private $expected_args;
+	private $negation;
 
-	public function __construct( $args ) {
+	public function __construct( $args, $negation = false ) {
 		parent::__construct();
 		$this->expected_args = $args;
+		$this->negation = $negation;
 	}
 
 	public function matches( $other ) {
 		if ( ! $other instanceof \Spies\Spy ) {
 			return false;
 		}
+		if ( $this->negation ) {
+			return ! $other->was_called_with_array( $this->expected_args );
+		}
 		return $other->was_called_with_array( $this->expected_args );
 	}
 
 	public function failureDescription( $other ) {
 		$generator = new FailureGenerator();
-		$generator->spy_was_not_called_with( $other, $this->expected_args );
+		$this->negation ?$generator->spy_was_called_with( $other, $this->expected_args ) : $generator->spy_was_not_called_with( $other, $this->expected_args );
 		return $generator->get_message();
 	}
 
